49aae8b4cf0d86cc11ccfd29de55364d4c8a23a3
D.L <david.landa@protonmail.com>
2023-10-27 11:45:15 +0200

    Cleanup and script documentation

--- labeler/build.sh
+++ labeler/build.sh
@@ -1,3 +1,9 @@
+# This script builds open-source NLP libraries/services 
+# It operates in the directory from which it is executed.
+
+
+# Download and build `ufal.korektor` server.
+
 cd lib
 git clone git@github.com:ufal/korektor.git
 cd korektor/src
@@ -6,12 +12,17 @@ mv korektor_server ../..
 cd ../../..
 rm -rf lib/korektor
 
+
+# Download `ufal.korektor` models.
+
 cd data
 curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-1460{/korektor-czech-130202.zip}
 unzip korektor-czech-130202.zip
 mv korektor-czech-130202 korektor_model
 rm korektor-czech-130202.zip
 cd ..
+
+# Download and build `ufal.morphodita` server.
 
 cd lib
 git clone git@github.com:ufal/morphodita.git
@@ -20,6 +31,8 @@ make server
 mv rest_server/morphodita_server ../..
 cd ../../..
 rm -rf lib/morphodita
+
+# Download `ufal.morphodita` models.
 
 cd data
 curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4794{/czech-morfflex2.0-pdtc1.0-220710.zip}
--- labeler/morphodita.sh
+++ labeler/morphodita.sh
@@ -1,1 +1,3 @@
+# Run `ufal.morphodita` service
+
 ./lib/morphodita_server 3000 model data/morphodita_model/czech-morfflex2.0-220710.dict README.md
